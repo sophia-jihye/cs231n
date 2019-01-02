3e4371e72abefa589ab3c445faa7d8826f8b75ca
sophia-jihye <jihyeparkk@naver.com>
2019-01-02 15:44:09 +0900

    initial commit for assignment2

--- assignment2/cs231n/layers.py
+++ assignment2/cs231n/layers.py
@@ -25,7 +25,7 @@ def affine_forward(x, w, b):
     # TODO: Implement the affine forward pass. Store the result in out. You   #
     # will need to reshape the input into rows.                               #
     ###########################################################################
-    pass
+    out = np.dot(x,w) + b
     ###########################################################################
     #                             END OF YOUR CODE                            #
     ###########################################################################
